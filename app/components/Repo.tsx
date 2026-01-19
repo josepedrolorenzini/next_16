108ff2f8a4bd9de9297772e85c31337119e8034b
josepedrolorenzini <webmaster@marbleceramiccorp.com.au>
2026-01-20 10:52:11 +1100

    app/components/repo - card icons tailwindcss

--- app/components/Repo.tsx
+++ app/components/Repo.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 import { FaStar, FaCodeBranch, FaEye } from 'react-icons/fa';
-async function fetchRepo(name: string): Promise<String> {
+
+async function fetchRepo(name: string): Promise<any> {
     const response = await fetch( `https://api.github.com/repos/josepedrolorenzini/${name}`, 
         {
         headers:{
@@ -20,7 +21,24 @@ const Repo = async ({name}: {name: string}) => {
     const repo = await fetchRepo(name);
     console.log(repo);
   return (
-    <div>Repo: {name}</div>
+    <>
+       <h2>{repo.name}</h2>
+      <p>{repo.description}</p>
+      <div className='card-stats'>
+        <div className='card-stat'>
+          <FaStar />
+          <span>{repo.stargazers_count}</span>
+        </div>
+        <div className='card-stat'>
+          <FaCodeBranch />
+          <span>{repo.forks_count}</span>
+        </div>
+        <div className='card-stat'>
+          <FaEye />
+          <span>{repo.watchers_count}</span>
+        </div>
+      </div>
+    </>
   )
 }
 
